929c7af20c63d5685902abfcddbb68cbec3e6664
xiaodong ma <mxd_neu@163.com>
2020-06-29 23:55:01 +0800

    koa test

---
+++ koa/index.js
@@ -0,0 +1,29 @@
+// koa api
+
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/', (ctx, next) => {
+  ctx.body = {
+    1: 'suck',
+  }
+});
+
+
+router.get('/get/:id', (ctx, next) => {
+  let { id }= ctx.params;
+  ctx.body = {
+    id,
+    code: 1,
+  }
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+console.log('run----');
+app.listen(3000);
